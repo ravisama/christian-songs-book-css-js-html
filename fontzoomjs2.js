9cb2db3b5e6932cca5fe3eaceb2766e17a103f7a
ravisama <pastorravisama@gmail.com>
2017-03-06 23:18:39 +0530

    Create fontzoomjs2.js

---
+++ fontzoomjs2.js
@@ -0,0 +1,20 @@
+angular.module('textSizeSlider', [])
+  .directive('textSizeSlider', ['$document', function ($document) {
+    return {
+      restrict: 'E',
+      template: '<div class="text-size-slider"><span class="small-letter" ng-style="{ fontSize: min + unit }">A</span> <input type="range" min="{{ min }}" max="{{ max }}" step="{{ step || 0 }}" ng-model="textSize" class="slider" value="{{ value }}" /> <span class="big-letter" ng-style="{ fontSize: max + unit }">A</span></div>',
+      scope: {
+        min: '@',
+        max: '@',
+        unit: '@',
+        value: '@',
+        step: '@'
+      },
+      link: function (scope, element, attr) {
+        scope.textSize = scope.value;
+        scope.$watch('textSize', function (size) {
+          $document[0].body.style.fontSize = size + scope.unit;
+        });
+      }
+    }
+  }]);
